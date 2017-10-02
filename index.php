3466b37fc688c63969376fe61d8fd7658c16fa74
Chris3225 <christoph.quinten@rwth-aachen.de>
2017-10-02 15:17:09 +0200

    added add function

--- index.php
+++ index.php
@@ -10,9 +10,10 @@
 
     <body>
     <div id="container">
-        <h1>My to-Do List</h1>
+        <h1> </h1>
         <ul id="tabs">
             <li id="todo_tab" class="selected"><a href="#">To-Do</a></li>
+            <li id="add_tab"><a href="#">Add Entry</a></li>
         </ul>
         <div id="main">
             <div id="todo">
@@ -46,7 +47,23 @@ EOD;
                 ?> 
             </div>
             <div id="addNewEntry">
-                ...
+                <hr />
+                <h2>Add New Entry</h2>
+                <form action="addItem.php" method="post">
+                    <p>
+                        <label for="title"> Title</label>
+                        <input type="text" name="title" id="title" class="input"/>
+                    </p>
+
+                    <p>
+                        <label for="description"> Description</label>
+                        <textarea name="description" id="description" rows="10" cols="35"></textarea>
+                    </p>
+
+                    <p>
+                        <input type="submit" name="addEntry" id="addEntry" value="Add New Entry" />
+                    </p>
+                </form>
             </div>
         </div>
     </div>
